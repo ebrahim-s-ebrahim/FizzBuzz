3bf6f268fde812998478cb6dbc9a4a935cde8540
ebrahim-s-ebrahim <ebrahim3adel@gmail.com>
2023-03-30 00:10:02 +0200

    Add project files.

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System;
+using System.Text;
+
+Dictionary<int, string> fizzBuzzMapping = new Dictionary<int, string>
+        {
+            { 3, "Fizz" },
+            { 5, "Buzz" }
+        };
+
+for (int i = 1; i <= 100; i++)
+{
+    string output = "";
+
+    foreach (KeyValuePair<int, string> entry in fizzBuzzMapping)
+    {
+        if (i % entry.Key == 0)
+        {
+            output += entry.Value;
+        }
+    }
+
+    if (string.IsNullOrEmpty(output))
+    {
+        output = i.ToString();
+    }
+
+    Console.WriteLine(output);
+}
